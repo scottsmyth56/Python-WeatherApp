d2133457e47831f283a372f5200c8a3cfd3fb12a
Scott Smyth <smythscott60@gmail.com>
2023-02-13 10:22:18 +0000

    Add Connection to AWS Database

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USER = os.environ.get("DB_USER")
+PASSWORD = os.environ.get("DB_PASSWORD")
+PORT = os.environ.get("DB_PORT")
+HOST = os.environ.get("DB_HOST")
+NAME = os.environ.get("DB_NAME")
+
+conn = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    port=PORT,
+    database=NAME
+    )
+
+cursor = conn.cursor()
