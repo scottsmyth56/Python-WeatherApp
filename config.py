9d3febf2c1d5850771480a0a70b85a2f86291e46
Scott Smyth <smythscott60@gmail.com>
2024-01-09 11:14:07 +0000

    - Update Config vars

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USER = os.environ.get("DB_USER")
-PASSWORD = os.environ.get("DB_PASSWORD")
-PORT = os.environ.get("DB_PORT")
+#USER = os.environ.get("DB_USER")
+#PASSWORD = os.environ.get("DB_PASSWORD")
+#PORT = os.environ.get("DB_PORT")
 HOST = os.environ.get("DATABASE_URL")
-NAME = os.environ.get("DB_NAME")
+#NAME = os.environ.get("DB_NAME")
 API_KEY = os.environ.get("API_KEY")
